5ad79358b2e231c049c2948fa1f598ff91d2b9c7
Quynh Chi Lai <quynhchi1009@yandex.com>
2023-03-23 13:30:59 +0100

    Enhanced object literals

--- Docs/ES6+/EnhancedObjectLiterals.js
+++ Docs/ES6+/EnhancedObjectLiterals.js
@@ -2,3 +2,20 @@
 //2. Dinh nghia method cho object
 //3. Dinh nghia key cho object duoi dang bien
 
+var name = "JavaScript Basic";
+var price = 100;
+var course = {
+    name,
+    price,
+    getName() {
+        return name;
+    }
+};
+console.log(course.getName());
+
+var fieldname = "Name";
+var fiedPrice = "Price";
+var course = {
+    [fieldname]: name,
+    [fiedPrice]: 1000
+}
